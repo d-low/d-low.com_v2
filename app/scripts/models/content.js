c1b507e0d4625d9994d2cc24f737d4e8044e0e79
Mike DiLorenzo <mike.dilorenzo@gmail.com>
2014-08-14 23:27:26 -0600

    Populate content title and titleNav for use in breadcrumb.

--- app/scripts/models/content.js
+++ app/scripts/models/content.js
@@ -18,7 +18,48 @@ Dlow.Models = Dlow.Models || {};
             this.set("subcontents", []);
             this.set("posts", []);
             this.set("url", "/#content/" + this.get("path"));
-            // TODO: this.set("title", ...);
+
+            // Parse the title creating a hash of parts to URLs so that we can
+            // display a breadcrumb letting the user know where they are and 
+            // making each part an anchor for easier navigation.
+    
+            var parts = this.get("path").split("/");
+            var title = [];
+            var titleNav = [];
+
+            parts.forEach(function(part, i) {
+                var name = part.replace(/^\d\d-/, "").replace(/[-_]/g, " ");
+                
+                // Remove the name of the previous part of the title from the
+                // current part of the title, i.e. chnage "Colorado / Colorado 
+                // 2012" to "Colorado / 2012".
+
+                if (typeof title[i -1] !== "undefined") {
+                    name = name.replace(title[i - 1], "");
+                }
+
+                title.push(name);
+
+                // Now create the name/url pairs that will be used on the desktop
+                // site for a breadcrumb.
+
+                var url = "";
+
+                if (i == 0 && i != parts.length - 1) {
+                    url = "/#content/" + parts[0];
+                }
+                else if (i != parts.length - 1) {
+                    url = "/#content/" + parts.slice(0, i + 1).join("/");
+                }
+
+                titleNav.push({
+                    name: name,
+                    url: url
+                });
+            });
+
+            this.set("title", title.join(" / "));
+            this.set("titleNav", titleNav);
 
             // Find our content in the content data structure based on the path 
             // passed to us.  When called for the top level node our path will
